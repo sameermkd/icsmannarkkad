49ac4576d6cc51f33f19462c992aef5e76347b29
Sameer Kongath <zoomixinfo@gmail.com>
2023-10-01 11:27:27 +0530

    Attendence Systems Added

--- attendence/urls.py
+++ attendence/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     path("",views.index, name="attendence/index"),
-    path("generate",views.generate, name="attendence/index")
+    path("generate",views.generate, name="attendence/generate"),
+    path("getdata/",views.getdata, name="attendence/getdata"),
+    path("leave/",views.leave, name="attendence/leave"),
 ]
--- attendence/views.py
+++ attendence/views.py
@@ -3,9 +3,30 @@ from data.models import Student
 from .utils import render_to_pdf
 from dateutil.parser import parse
 from dateutil.rrule import rrule, DAILY, MO, TU, WE, TH, FR, SA
+from . models import Attendence
 
 def index(request):
-    return render(request,"attendence/index.html")
+    if request.method=='POST':
+        a=request.POST.getlist("invoiceno"),
+        for invoiceno, name, course, attendence, phone1, phone2 in zip(
+        request.POST.getlist("invoiceno"),
+        request.POST.getlist("name"),
+        request.POST.getlist("course"),
+        request.POST.getlist("attendence"),
+        request.POST.getlist("mob1"),
+        request.POST.getlist("mob2"),
+        ):
+            ex = Attendence.objects.create(
+            invoiceno=invoiceno,
+            name=name,
+            course=course,
+            attendence=attendence,
+            mob1=phone1,
+            mob2=phone2,
+            )
+            ex.save()
+        return render(request, "attendence/index.html")
+    return render(request, "attendence/index.html")
 def generate(request):
     date=[]
     exclude=["complete","drop","break"]
@@ -13,7 +34,6 @@ def generate(request):
         start=request.POST['startingdate']
         end=request.POST['endingdate'] 
         batch=request.POST['batch']
-        print(batch)
         if batch=='saturday':
             student=Student.objects.filter(batchtime=batch).exclude(staus__in=exclude)
             date_list = rrule(
@@ -57,3 +77,21 @@ def generate(request):
             },
             )
     return render(request,"attendence/generate.html")
+def getdata(request):
+    exclude=["complete","drop","break"]
+    batch=request.GET['batch']
+    student=Student.objects.filter(batchtime=batch).exclude(staus__in=exclude)
+    context={
+        "student":student
+        }
+    return render(request,"attendence/getdata.html",context)
+def leave(request):
+    exclude=["complete","drop","break"]
+    if request.method=='POST':
+        date=request.POST['date']
+        student=Attendence.objects.filter(date=date).filter(attendence="leave")
+        context={
+            "student":student
+            }
+        return render(request,"attendence/leave.html",context)
+    return render(request,"attendence/leave.html")
